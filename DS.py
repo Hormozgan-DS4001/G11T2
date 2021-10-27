dbd25c1b9cde45532a4b63d90a8b2b20ebc07587
kamandkrg <kamandkargar@gmail.com>
2021-10-27 16:52:35 +0330

    add Ds

---
+++ DS.py
@@ -0,0 +1,28 @@
+class Suggestion:
+    def __init__(self):
+        pass
+
+
+class User:
+    def __init__(self):
+        pass
+
+
+class Store:
+    def __init__(self):
+        pass
+
+
+class Core:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
