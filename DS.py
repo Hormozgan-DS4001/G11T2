01f401e8840f6af7a4e2efa7ed73ef35ee166956
mili <m.ahrari77@gmail.com>
2021-10-30 11:57:07 +0330

    added methods for Manager and Store

--- DS.py
+++ DS.py
@@ -1,10 +1,33 @@
+from data_structure.Structure import SArray
+
 class Suggestion:
     def __init__(self):
         pass
 
 
 class Manager:
+    stores = SArray(200)
+    resellers = SArray(200)
+
     def __init__(self):
+        pass
+
+    def search_store(self):
+        pass
+
+    def add_reseller(self):
+        pass
+
+    def create_reseller(self):
+        pass
+
+    def delete_reseller(self):
+        pass
+
+    def create_store(self):
+        pass        
+
+    def view_sugesstion(self):
         pass
 
 
@@ -20,7 +43,21 @@ class Reseller:
 
 
 class Store:
+    id = 0
+
     def __init__(self):
+        pass
+
+    def add_reseller(self):
+        pass
+
+    def create_reseller(self):
+        pass
+
+    def delete_reseller(self):
+        pass
+
+    def search(self):
         pass
 
 
