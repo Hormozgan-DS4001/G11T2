ba521211ad3bd3d79ed93c381e66f763a0c2b23c
kamandkrg <kamandkargar@gmail.com>
2021-11-02 21:26:00 +0330

    completing method in DS and start GUI_Reseller

--- DS.py
+++ DS.py
@@ -9,21 +9,6 @@ class Suggestion:
         self.is_delete = False
 
 
-class Manager:
-
-    def __init__(self, name, national_code, password):
-        self.name = name
-        self.national_code = national_code
-        self.password = password
-        self.suggestion_list = Dll()
-
-    def view_suggestion(self):
-        return self.suggestion_list.get_node_handler(len(self.suggestion_list) - 1)
-
-    def add_suggestion_manager(self, suggestion: "Suggestion"):
-        self.suggestion_list.append(suggestion)
-
-
 class Reseller:
 
     def __init__(self, name: str, national_code: int, password: str = "123"):
@@ -33,14 +18,14 @@ class Reseller:
         self.suggestion_user = DArray()
         self.stores_list = Dll()
 
-    def add_suggestion(self):
-        pass
+    def add_suggestion(self, suggestion: "Suggestion"):
+        self.suggestion_user.append(suggestion)
 
     def view_suggestion(self):
-        pass
+        return self.suggestion_user.IndexHandler(self.suggestion_user, len(self.suggestion_user) - 1)
 
     def show_stores_user(self):
-        pass
+        return self.suggestion_user.IndexHandler(self.stores_list, 0)
 
 
 class Store:
@@ -66,27 +51,38 @@ class Store:
 class Core:
 
     def __init__(self):
+        self.suggestion_list = Dll()
         self.stores = SArray(200)
-        self.resellers = SArray(200)
+        self.resellers = DArray(200)
         self.number_reseller = -1
 
-    def create_new_reseller(self):
-        pass
+    def create_new_reseller(self, name, national_code, password):
+        new_reseller = Reseller(name, national_code, password)
+        self.number_reseller += 1
+        self.resellers[self.number_reseller] = new_reseller
 
-    def delete_reseller(self):
-        pass
+    @staticmethod
+    def delete_reseller(store: "Store"):
+        store.delete_reseller()
 
     def show_stores(self):
-        pass
+        return self.stores.NodeHandler(self.suggestion_list, 0)
 
-    def search_store(self):
-        pass
+    def search_store(self, store_code: int):
+        return self.stores[store_code - 1]
 
-    def add_suggestion(self):
-        pass
+    def create_store(self, address, rant):
+        store = Store(address, rant)
+        self.stores[store.STORE_CODE - 1] = store
 
-    def create_store(self):
-        pass
+    def view_suggestion(self):
+        return self.suggestion_list.get_node_handler(len(self.suggestion_list) - 1)
+
+    def add_suggestion(self, reseller: "Reseller", text: str):
+        time = datetime.date.today().strftime("%b-%d-%Y")
+        suggestion = (text, time)
+        self.suggestion_list.append(suggestion)
+        reseller.suggestion_user.append(suggestion)
 
     def login(self, national_code, password):
         # use binary search
--- GUI_Reseller.py
+++ GUI_Reseller.py
@@ -2,7 +2,18 @@ from configure import Tk, Frame, Button, Entry, Label, LabelFrame
 
 
 class ResellerView(Tk):
-    pass
+    def __init__(self, callback_user, callback_show_stores,
+                 callback_add_suggestion, callback_show_suggestion
+                 ):
+        self.callback_user = callback_user
+        self.callback_show_stores = callback_show_stores
+        self.callback_show_suggestion = callback_show_suggestion
+        self.callback_add_sug = callback_add_suggestion
+
+        self.title("Reseller")
+        self.geometry("500x500+200+200")
+        self.resizable(False, False)
+
 
 
 
