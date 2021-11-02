8689c47f19ebd2b69888def88aa9cf9de754b029
kamandkrg <kamandkargar@gmail.com>
2021-11-03 01:01:02 +0330

    add suggestion view

--- GUI_Reseller.py
+++ GUI_Reseller.py
@@ -5,6 +5,8 @@ class ResellerView(Tk):
     def __init__(self, callback_user, callback_show_stores,
                  callback_add_suggestion, callback_show_suggestion
                  ):
+        super(ResellerView, self).__init__()
+
         self.callback_user = callback_user
         self.callback_show_stores = callback_show_stores
         self.callback_show_suggestion = callback_show_suggestion
@@ -14,6 +16,15 @@ class ResellerView(Tk):
         self.geometry("500x500+200+200")
         self.resizable(False, False)
 
+        frm1 = Frame(self)
+        frm1.grid(row=0, column=0)
+        Label(text=f"Name: {callback_user.name}").grid(row=0, column=0)
+        Label(text=f"National Code: {callback_user.national_code}").grid(row=1, column=0)
+
+        Button(self, text="Add Suggestion", command=self.sug_cmd)
+
+    def sug_cmd(self):
+        pass
 
 
 
